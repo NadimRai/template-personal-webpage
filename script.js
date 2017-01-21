505d6229fedac203a248091cf1dd0e0c7ad2d962
NadimRai <rai_nadim@yahoo.com>
2017-01-21 01:46:06 -0800

    Animated scrolling with jQuery

--- script.js
+++ script.js
@@ -3,10 +3,22 @@ $(document).ready(function(){
         $('.nav').slideToggle();
     })
     $(window).bind('resize', function () {
-                if ($('.navicon').css('display') == "none") {
-                    $('.nav').show();
-                } else {
-                    $('.nav').hide();
-                }
-            })
+        if ($('.navicon').css('display') == "none") {
+            $('.nav').show();
+        } else {
+            $('.nav').hide();
+        }
+    });
+
+    $('.nav li a').click(function (event) {
+        event.preventDefault();
+        var sectionId = $(this).attr('href');
+        var fromTop = $(sectionId).offset().top;
+        $('html, body').animate({
+            scrollTop: fromTop
+        }, 1000);
+        if ($('.navicon').css('display') == 'block') {
+            $('.nav').slideUp();
+        }
+    });
 });
